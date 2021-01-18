4cb86cba54651c028173ced86ce4090ea29434e4
lshw54 <61426712+lshw54@users.noreply.github.com>
2021-01-19 00:20:52 +0800

    Initial commit

---
+++ calibrate_the_vehicle.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+import time
+import deepracer_vehicle_api
+
+# ####################################################################
+# Load the configuration
+#
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(dir_path, "config.yaml"), "r") as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+# ####################################################################
+# Create the API client and login
+
+client = deepracer_vehicle_api.Client(cfg["password"], cfg["ip"])
+
+# ####################################################################
+#
+print(client.set_calibration_mode())
+
+print(client.set_calibration_throttle(cfg["calibration"]["throttle"]))
+print(client.set_calibration_angle(cfg["calibration"]["angle"]))
+
+print("Throttle", client.get_calibration_throttle())
+print("Angle", client.get_calibration_angle())
+
+
+client.start_car()
+client.move_forward(timeout=1.7)
